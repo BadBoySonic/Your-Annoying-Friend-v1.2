d7054f9e30b41769dadadfcb6dd61deb0e9e9fec
BadBoySonic <150901959+BadBoySonic@users.noreply.github.com>
2024-04-18 20:49:16 +0530

    Add files via upload

---
+++ myfirstproje.py
@@ -0,0 +1,12 @@
+q1 = input("Hello! What Is Your Name? ")
+q2 = input("What Is Your Age? ")
+print("You are",q1,"Age:",q2,)
+print("One More Thing.....")
+
+q3 = input("Do you Like Cats? Yes or No?")
+
+if q3 >= "Yes":
+  print("Nice!")
+
+else: 
+    print("I Hate You!")
